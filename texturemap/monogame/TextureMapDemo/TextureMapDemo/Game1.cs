5a0473eee4a325f22b5aa3f6999052f22d1d0f46
Levi D. Smith <support@levidsmith.com>
2024-02-24 03:26:12 -0500

    MonoGame example code

---
+++ texturemap/monogame/TextureMapDemo/TextureMapDemo/Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TextureMapDemo {
+    public class Game1 : Game {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+
+        Model model;
+
+        public Game1() {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(-3f, 2f, -5f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45f), _graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 10f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, new Vector3(0f, 1f, 0f));
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
+
+            model = Content.Load<Model>("tennessee_box");
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            foreach (ModelMesh mesh in model.Meshes) {
+                foreach(BasicEffect effect in mesh.Effects) {
+                    effect.AmbientLightColor = new Vector3(1f, 0f, 0f);
+                    effect.View = viewMatrix;
+                    effect.World = worldMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ texturemap/monogame/TextureMapDemo/TextureMapDemo/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new TextureMapDemo.Game1();
+game.Run();
